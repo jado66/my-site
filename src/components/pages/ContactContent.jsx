030d7f1ccc44bb8007986ce97da81a734c103de8
jado66 <77120464+jado66@users.noreply.github.com>
2023-02-22 20:37:25 -0700

    Fixed margin

--- src/components/pages/ContactContent.jsx
+++ src/components/pages/ContactContent.jsx
@@ -2,14 +2,18 @@ import Link from "next/link"
 
 const ContactContent = () =>{
     return(
-        <div className="d-block gx-lg-5 px-lg-5 text-dark" >
+        <div className="d-block  text-dark" >
             <h1 className="text-center text-capitalize my-4">Contact Me</h1>
             
             <p className="text-center mb-5">Here is how you can contact me</p>
 
-            <div className="border border-dark rounded-3 mx-lg-auto col-lg-8 col-12 mx-2 py-5 text-center" style={{height:"15em"}}>
-                Here will be a form to send me an email
-            </div> 
+            <div className="px-2">
+                <div className="border border-dark rounded-3 mx-lg-auto col-lg-8 col-12  py-5 text-center" style={{height:"15em"}}>
+                    Here will be a form to send me an email
+                </div>
+            </div>
+
+            
            
 
            
