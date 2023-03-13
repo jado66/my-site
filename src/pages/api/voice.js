f95a5ecd9610d5e012e0551a2af10ac0381cf652
jado66 <77120464+jado66@users.noreply.github.com>
2023-03-13 13:19:46 -0600

    Made api work for all methods

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -3,7 +3,7 @@ const VoiceResponse = twiml.VoiceResponse;
 
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {
+  // if (req.method === 'POST') {
     // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
     twiml.say({ voice: 'alice' }, 'hello world!');
@@ -11,7 +11,7 @@ export default function handler(req, res) {
     // Render the response as XML in reply to the webhook request
     res.setHeader('Content-Type', 'text/xml');
     res.status(200).send(twiml.toString());
-  } else {
-    res.status(405).end(); // Method Not Allowed
-  }
+  // } else {
+  //   res.status(405).end(); // Method Not Allowed
+  // }
 }
