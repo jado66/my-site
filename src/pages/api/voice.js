21a7b0507bebed31811ad669af1813acc2dc7ade
jado66 <77120464+jado66@users.noreply.github.com>
2023-03-13 13:36:46 -0600

    Parror API tweak

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -1,17 +1,29 @@
 import { twiml } from 'twilio';
 const VoiceResponse = twiml.VoiceResponse;
 
-
 export default function handler(req, res) {
   // if (req.method === 'POST') {
     // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
-    twiml.say({ voice: 'alice' }, 'hello world!');
 
+    // Check if there is a speech result from the caller
+    if (req.body.SpeechResult) {
+      // Repeat the speech result back to the caller
+      twiml.say({ voice: 'alice' }, req.body.SpeechResult);
+    }
+  
+    // Gather speech input from the caller
+    const gather = twiml.gather({
+      input: 'speech',
+      timeout: 5,
+      speechTimeout: 'auto'
+    });
+  
+    // Say a prompt to the caller
+    gather.say({ voice: 'alice' }, 'Please say something and I will repeat it back to you.');
+  
     // Render the response as XML in reply to the webhook request
     res.setHeader('Content-Type', 'text/xml');
     res.status(200).send(twiml.toString());
-  // } else {
-  //   res.status(405).end(); // Method Not Allowed
-  // }
+  
 }
