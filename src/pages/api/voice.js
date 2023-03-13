5ad01bd0e9f4f97357a41cdea0165fc5d935b5ef
jado66 <77120464+jado66@users.noreply.github.com>
2023-03-13 14:03:21 -0600

    Integrated with chatgpt

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -1,15 +1,37 @@
 import { twiml } from 'twilio';
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const VoiceResponse = twiml.VoiceResponse;
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   // if (req.method === 'POST') {
     // Use the Twilio Node.js SDK to build an XML response
    const twiml = new VoiceResponse();
 
+
+
+
   // Check if there is a speech result from the caller
   if (req.body.SpeechResult) {
     // Repeat the speech result back to the caller
-    twiml.say({ voice: 'alice' }, req.body.SpeechResult);
+
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: req.body.SpeechResult,
+      temperature: 0.9,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 3500,
+    });
+
+    const output = completion.choices[0].text
+
+    twiml.say({ voice: 'alice' }, output);
   }
 
   // Gather speech input from the caller
