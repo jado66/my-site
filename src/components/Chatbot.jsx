4ffb7466c07910d5f318d4b1910c08051cd5875f
jado66 <77120464+jado66@users.noreply.github.com>
2023-04-07 15:41:34 -0600

    Added AMY chatbot

---
+++ src/components/Chatbot.jsx
@@ -0,0 +1,105 @@
+import { useState, useRef } from "react";
+// Import styles from chat-ui-kit-styles
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+// Import components from chat-ui-kit-react
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator, MessageSeparator, Avatar, localSender, SendButton, AttachmentButton, InfoButton, MessageGroup, CustomMessage } from "@chatscope/chat-ui-kit-react";
+
+const Chatbot = () => {
+
+    const [isMinimized, setIsMinimized] = useState(true);
+    const [isAmyTyping, setIsAmyTyping] = useState(false);
+    const lastIdRef = useRef(0);
+    const inputRef = useRef();
+    const [msgInputValue, setMsgInputValue] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const handleSend = message => {
+        setIsAmyTyping(true);
+        setMessages([...messages, {
+          message,
+          direction: 'outgoing'
+        }]);
+        setMsgInputValue("");
+        inputRef.current.focus();
+      };
+    
+
+    const handleChatbotMessage = (e) => {
+        setChatbotMessage(e.target.value);
+    };
+    
+    const handleChatbotResponse = () => {
+        setChatbotResponse(chatbotMessage);
+    };
+    
+    if (isMinimized) {
+        return (
+        <div>
+            <button className="btn" onClick={() => setIsMinimized(false)} style = {{width: '150px', height: '150px'}} >
+                <img 
+                    src='AmyAvatar.png' 
+                    className='img-fluid ps-lg-5 mx-lg-0 mx-auto my-auto' 
+                    alt='chatbot'
+                    
+                />
+            </button>
+        </div>
+        );
+    }
+
+
+    return (
+        <div className = "box-shadow  me-4 mt-4" onBlur={()=>setIsMinimized(false)} 
+            style = {{minWidth:"300px", height:"450px"}}
+        >
+            <MainContainer >
+                <ChatContainer  >
+                <MessageList scrollBehavior="smooth" >
+                <Message model={{
+                    message: "Hello and welcome to J-Apps. What can I help you with?",
+                    sentTime: "15 mins ago",
+                    sender: "Amy",
+                    direction: "incoming",
+                    position: "single"
+                }}>
+                        <Avatar src={'AmyAvatar.png'} name={"Amy"} />
+                    </Message>
+                    {messages.map(m => <Message key={m.id} model={m} />)}
+                    {isAmyTyping&&<TypingIndicator content="Amy is typing" />}
+                </MessageList>
+                <MessageInput placeholder="Type message here" onSend={m => handleSend(m, localSender)} onChange={setMsgInputValue} value={msgInputValue} ref={inputRef} />
+                    <div as={MessageInput} style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        borderTop: "1px dashed #d1dbe4"
+                    }}>
+                        <MessageInput ref={inputRef} onChange={msg => setMsgInputValue(msg)} value={msgInputValue} sendButton={false} attachButton={false} onSend={handleSend} style={{
+                            flexGrow: 1,
+                            borderTop: 0,
+                            flexShrink: "initial"
+                            }} />                                
+                        <SendButton onClick={() => handleSend(msgInputValue, localSender)} disabled={msgInputValue.length === 0} style={{
+                            fontSize: "1.2em",
+                            marginLeft: 0,
+                            paddingLeft: "0.2em",
+                            paddingRight: "0.2em"
+                            }} />
+                            {/* <AttachmentButton style={{
+                                fontSize: "1.2em",
+                                paddingLeft: "0.2em",
+                                paddingRight: "0.2em"
+                                }} /> */}
+                        <InfoButton onClick={() => alert("Important message!")} style={{
+                            fontSize: "1.2em",
+                            paddingLeft: "0.2em",
+                            paddingRight: "0.2em"
+                            }} /> 
+                    </div>
+                        
+                </ChatContainer>
+            </MainContainer>
+        </div>
+    );
+}
+
+export default Chatbot
--- src/pages/amy.jsx
+++ src/pages/amy.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import { Fade } from "react-reveal"
+import Chatbot from '@/components/Chatbot';
 
 const Amy = () => {
   
@@ -21,8 +22,10 @@ const Amy = () => {
         <Head>
             <title>AMY - AutoMate Your </title>
         </Head>
-        <div className='container-fluid px-0 pb-lg-5 pb-2 col-lg-8 col-12 px-lg-0 px-3'>
-            
+        <div className='container-fluid px-0 pb-lg-5 pb-2 col-lg-8 col-12 px-lg-0 px-3 position-relative'>
+
+        
+
         <Card className='d-flex flex-column mb-4 text-center mt-lg-5 mt-3 p-5' noFade>
             <h1>Introducing Your Personal AI Assistant - AMY </h1>
             <hr/>
@@ -34,9 +37,9 @@ const Amy = () => {
             </p>
         </Card>
 
-        <Card className='d-flex flex-lg-row flex-column mb-4' bottom> 
+        <Card className='d-flex flex-lg-row flex-column mb-4 bg-amy-light' bottom> 
             <div className='col-lg-5 col-12 d-flex mt-lg-0 mt-5'>
-                <img src='Amy.png' className='img-fluid ps-lg-5 mx-lg-0 mx-auto my-auto' alt='chatbot'/>
+                <img src='Amy.svg' className='img-fluid ps-lg-5 mx-lg-0 mx-auto my-auto' alt='chatbot'/>
             </div>
             <div className='col-lg-7 col-12 p-5'>
                 <h2 className="text-center mt-4 mb-5">Introducing AMY - Your Personal AI Assistant</h2>
@@ -44,12 +47,12 @@ const Amy = () => {
             </div>  
         </Card>
 
-        <Card className='d-flex flex-column mb-4 p-5' left> 
+        <Card className='d-flex flex-column mb-4 p-5 bg-amy-4 ' left> 
             <h2 className="text-center mt-4 mb-3">What is AMY?</h2>
             <p className='fs-5'>AMY is a powerful AI platform that offers customized solutions to the unique needs of your business. Using trillions of parameters, AMY delivers a human-like experience by learning from your website and data. AMY can connect with your customer and handle their requests with ease through a chat module, phone call system and text message service.</p>
         </Card>
 
-        <Card className='d-flex flex-column mb-4 p-5' right>
+        <Card className='d-flex flex-column mb-4 p-5 bg-amy-2' right>
         <div className='flex-column mb-4'>
                 <h2 className="text-center mt-4 mb-3">AMY's Capabilities</h2>
                 <p className='fs-5'>AMY can provide a range of services for your business from managing appointments, processing orders and providing exceptional support to your customers. AMY's capabilities are vast and can be tailored to meet the unique needs of your business.</p>
@@ -73,8 +76,8 @@ const Amy = () => {
             </div>
         </Card>
 
-        <Card className='d-block column mb-4 p-5' bottom> 
-            <h2 className="text-center mt-4 mb-5">Benefits of Using AMY</h2>
+        <Card className='d-block column mb-4 p-5 bg-amy-1' bottom> 
+            <h2 className="text-center mt-4 mb-5 text-amy-light">Benefits of Using AMY</h2>
             <div class="row g-3">
             <div class="col-lg-6 col-12">
                 <div className='p-3 rounded-3 border border-dark h-100 text-start' style={{backgroundColor:"#DCDDEF"}}>
@@ -103,30 +106,34 @@ const Amy = () => {
             </div>
         </Card>
 
-        <Card className='d-flex flex-lg-row flex-column mb-4' left> 
+        <Card className='d-flex flex-lg-row flex-column mb-4 bg-amy-3' left> 
             <div className='col-lg-9 col-12 p-5'>
             <h2 className="text-center mt-4 mb-5">Monitoring Performance</h2>
             <p className='fs-5'>AMY comes with powerful analytics tools, which help you monitor and measure your assistant's performance seamlessly. From response time to customer satisfaction, churn rate, engagement rates and much more, you can now measure the impact of your assistant and refine it as quickly and frequently as needed based on data-driven decision making.</p>
             </div>  
-            <div className='col-lg-3 col-12 d-flex'>
-            <div className=' flex-grow-1' style={{backgroundColor:"#DCDDEF"}}/>
+            <div className='col-lg-3 col-12 d-flex p-2'>
+                <div className=' flex-grow-1 bg-amy-light border rounded' />
             </div>
         </Card>
 
-        <Card className='d-flex flex-lg-row flex-column mb-4' right> 
-            <div className='col-lg-3 col-12 d-flex'>
-            <div className=' flex-grow-1' style={{backgroundColor:"#DCDDEF"}}/>
+        <Card className='d-flex flex-lg-row flex-column mb-4 bg-amy-1 text-amy-light' right> 
+            <div className='col-lg-3 col-12 d-flex p-2'>
+                <div className=' flex-grow-1 bg-amy-light border rounded' />
             </div>
             <div className='col-lg-9 col-12 p-5'>
-            <h2 className="text-center mt-4 mb-5">Partner With Us</h2>
-            <p className='fs-5'>Take your business to the next level with AMY, the most advanced and reliable AI assistant for your customer service and business operations. Sign up today and receive a life-changing assistance experience that will improve your customer satisfaction and increase sales for your business.</p>
-            <button className="btn btn-info">Get Started Now</button>
+                <h2 className="text-center mt-4 mb-5">Partner With Us</h2>
+                <p className='fs-5'>Take your business to the next level with AMY, the most advanced and reliable AI assistant for your customer service and business operations. Sign up today and receive a life-changing assistance experience that will improve your customer satisfaction and increase sales for your business.</p>
+                <button className="btn btn-info text-amy-light ">Get Started Now</button>
             </div>  
         </Card>
-                    
+              
         </div>
 
-        </>
+       
+            <div className='position-absolute top-0 end-0'>
+                <Chatbot/>
+            </div>  
+        </> 
     );
 };
 
